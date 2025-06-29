ea3e7169f183746a051801e65c59ef1fef03608e
SonixDev131 <son.nl215131@gmail.com>
2025-06-29 21:50:15 +0000

    Config fullbase url

--- cake-app/config/app.php
+++ cake-app/config/app.php
@@ -56,7 +56,7 @@ return [
         'webroot' => 'webroot',
         'wwwRoot' => WWW_ROOT,
         //'baseUrl' => env('SCRIPT_NAME'),
-        'fullBaseUrl' => false,
+        'fullBaseUrl' => 'https://cakephp.local:3443',
         'imageBaseUrl' => 'img/',
         'cssBaseUrl' => 'css/',
         'jsBaseUrl' => 'js/',
